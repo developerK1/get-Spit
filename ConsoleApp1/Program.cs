df596fa157f8947866d688910667cc79f8d64e5e
Kekaone <maobaKG@yahoo.com>
2023-03-21 21:18:26 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArr= new String[4];
+            int counter = 0;
+            Console.WriteLine("Add a List...");
+
+            while(counter < 4)
+            {
+                myArr[counter] = Console.ReadLine();
+                
+
+                if (counter < 3)
+                {
+                    Console.WriteLine("Add the Last one");
+                }
+                else
+                {
+                    Console.WriteLine("Add another one");
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine("Here is your list...");
+
+ 
+            for(int idx = 0; idx < 4; idx++)
+            {
+                Console.WriteLine(myArr[idx]);
+            }
+
+            Console.ReadKey();
+;        }
+    }
+}
